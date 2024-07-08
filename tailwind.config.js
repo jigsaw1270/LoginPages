15fb2d127f170cffba6dc9b61c7bde243bc17241
jigsaw1270 <nomadshomoy@gmail.com>
2024-07-09 00:21:31 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      backgroundImage : {
+          "yacht": "url('/images/yacht2.jpg')"
+
+      },
+    
+    },
+  },
+  plugins: [],
+}
+
